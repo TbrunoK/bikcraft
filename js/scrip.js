f12c6c233425dd58e6ed8057889ebe19f39722cf
TbrunoK <tbrunok@hotmail.com>
2024-06-24 00:44:10 -0300

    Adicionado = função para marcar o tipo de seguro escolhido na hora da compra

--- js/scrip.js
+++ js/scrip.js
@@ -1,14 +1,36 @@
+// selecionar os links
 const links = document.querySelectorAll(".header-menu a")
 
+// Função para ativar links de navegação
+//  Esta função adiciona a classe "ativo" a um link, se a URL atual da página
+//  incluir o href do link fornecido. Isto é útil para destacar o link
+//  correspondente à página atual na navegação do site.
+
 function ativarLinks(link) {
+  // Obtém a URL atual da página
   const url = location.href
+  // Obtém o href do link fornecido
   const href = link.href
 
-  url.includes
-
+  // Verifica se a URL atual inclui o href do link
   if (url.includes(href)) {
+    // Adiciona a classe "ativo" ao link para destacá-lo
     link.classList.add("ativo")
   }
 }
-
+// Aplica a função ativarLinks a cada link
 links.forEach(ativarLinks)
+
+// ativar itens do orçamento
+
+const parametros = new URLSearchParams(location.search)
+
+//
+function ativarProduto(parametro) {
+  const elemento = document.getElementById(parametro)
+  if (elemento) {
+    elemento.checked = true
+  }
+}
+
+parametros.forEach(ativarProduto)
