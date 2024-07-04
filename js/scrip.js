25d6b45647a79948a8b898ea1300fdfd9a1d63e6
TbrunoK <tbrunok@hotmail.com>
2024-07-04 17:22:12 -0300

    Pequenas correções

--- js/scrip.js
+++ js/scrip.js
@@ -60,12 +60,11 @@ const galeriaContainer = document.querySelector(".bicicleta-imagens")
 function trocarImagem(event) {
   const img = event.currentTarget
   const media = matchMedia("(min-width: 1000px)").matches
+
   if (media) {
     galeriaContainer.prepend(img)
   }
 }
-
-trocarImagem()
 
 function eventosGaleria(img) {
   img.addEventListener("click", trocarImagem)
